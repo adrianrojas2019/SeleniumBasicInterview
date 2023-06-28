8136a2f044fc708403572f2b31279a2f12820808
adrianrojas2019 <rojasadrian@hotmail.com>
2023-06-27 18:31:50 -0600

    sample project with two test using selenium with c# and nunit framework

---
+++ SeleniumBasic1/SeleniumBasic1/Pages/GooglePage.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumBasic1
+{
+    class GooglePage
+    {
+        private IWebDriver driver;
+
+        public GooglePage(IWebDriver _driver)
+        {
+            driver = _driver;
+        }
+
+        //public IWebElement SearchBox => driver.FindElement(By.CssSelector("input[title*=\"Search\"]"));
+        public IWebElement SearchBox => driver.FindElement(By.Name("q"));
+        //or the same below approach get property
+
+        //public IWebElement SearchBox
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.CssSelector("input[value*='Buscar con Google']"));
+
+        //    }
+        //}
+
+        //public IWebElement SearchButton => driver.FindElement(By.CssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b"));
+        // getting two elements
+        //public IWebElement SearchButton => driver.FindElement(By.Name("btnK"));
+        public IWebElement SearchButton => driver.FindElement(By.CssSelector("div[class='FPdoLc lJ9FBc'] input[name='btnK']"));
+        
+        private string url = "http://www.google.com";
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl(url);
+            //Maximize current window
+            driver.Manage().Window.Maximize();
+        }
+
+        public bool IsOnResultsPage()
+        {
+            return driver.Url.Contains("/search");
+        }
+
+        public void SearchFor(string search)
+        {
+            SearchBox.SendKeys(search);
+            Thread.Sleep(100);
+            SearchBox.SendKeys(Keys.Escape);
+            Thread.Sleep(100);
+            SearchButton.Click();
+            //another workaround
+            //SearchBox.SendKeys(Keys.Enter);
+        }
+    }
+}
---
+++ SeleniumBasic1/SeleniumBasic1/Tests/GoogleTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SeleniumBasic1
+{
+    class GoogleTests : SeleniumTestBase
+    {
+        GooglePage googlePage;
+
+        [OneTimeSetUp]
+        public void SetupOnce()
+        {
+            googlePage = new GooglePage(driver);
+        }
+
+        [Test]
+        public void SearchForNothing()
+        {
+            //arrange
+            googlePage.GoToPage();
+
+            //act
+            googlePage.SearchFor("nothing");            
+
+            //assert
+            Assert.IsTrue(googlePage.IsOnResultsPage());
+        }
+
+        [Test]
+        public void LaunchChrome()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+
+            //Capture the Title
+            var title = driver.Title;
+            Console.WriteLine("Page Title: ");
+            Console.WriteLine(title);
+
+            var url = driver.Url;
+            Console.WriteLine("Page URL: ");
+            Console.WriteLine(url);
+
+
+            var pageSource = driver.PageSource;
+            Console.WriteLine("Page Source: ");
+            Console.WriteLine(pageSource);
+            Thread.Sleep(3000);
+
+            driver.Close();
+
+        }
+
+    }
+}
