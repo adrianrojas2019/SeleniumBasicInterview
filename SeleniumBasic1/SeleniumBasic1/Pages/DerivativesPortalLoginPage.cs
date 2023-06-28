c01e7da9297a6377411d795f6bee61dddc6a3604
adrianrojas2019 <rojasadrian@hotmail.com>
2023-06-28 15:29:17 -0600

    addInvalidCredentials script has been added

---
+++ SeleniumBasic1/SeleniumBasic1/Pages/DerivativesPortalLoginPage.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumBasic1
+{
+    class DerivativesPortalLoginPage
+    {
+        private IWebDriver driver;
+
+        public DerivativesPortalLoginPage(IWebDriver _driver)
+        {
+            driver = _driver;
+        }
+
+        public IWebElement LoginButton => driver.FindElement(By.XPath("(//a[contains(@class,'parametric-link')])[5]"));
+        //UserName
+        public IWebElement UserName => driver.FindElement(By.Id("ppaLoginUsername"));
+
+        //Password
+        public IWebElement Password => driver.FindElement(By.Id("ppaLoginPassword"));
+        //Login Portal Button
+        public IWebElement LoginPortalButton => driver.FindElement(By.CssSelector("button[class='btn btn-primary']"));
+        //Invalid error message
+        public IWebElement InvalidLoginCredentials => driver.FindElement(By.CssSelector(".text-danger.text-center.mb-3"));
+
+
+        private string url = "https://www.parametricportfolio.com/";
+
+        public bool IsOnResultsPage()
+        {
+            var allWindowHandles = driver.WindowHandles;
+
+            Thread.Sleep(2000);
+            driver.SwitchTo().Window(allWindowHandles[1]);
+            Thread.Sleep(2000);
+            return driver.Url.Contains("https://portal.parametricportfolio.com/login");
+        }
+
+        public void ClickOnLoginButton()
+        {
+            LoginButton.Click();
+        }
+
+        public string addInvalidCredentials(string userName, string password)
+        {
+            var allWindowHandles = driver.WindowHandles;
+
+            Thread.Sleep(2000);
+            driver.SwitchTo().Window(allWindowHandles[1]);
+            Thread.Sleep(2000);
+            //Get and send username
+            Console.WriteLine("TEST: " + userName);
+            UserName.SendKeys(userName);
+            Thread.Sleep(1000);
+            Password.SendKeys(password);
+            Thread.Sleep(1000);
+            //Clilck on Login Portal Button
+            LoginPortalButton.Click();
+            Thread.Sleep(1000);
+            return InvalidLoginCredentials.Text;
+        }
+    }
+}
---
+++ SeleniumBasic1/SeleniumBasic1/Tests/ParametricsLoginDerivativesPortal.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SeleniumBasic1
+{
+    class ParametricsLoginDerivativesPortal : SeleniumTestBase
+    {
+        HomeLoginPage loginPage;
+        DerivativesPortalLoginPage loginDerivativesPortal;
+
+        [OneTimeSetUp]
+        public void SetupOnce()
+        {
+            loginPage = new HomeLoginPage(driver);
+            loginDerivativesPortal = new DerivativesPortalLoginPage(driver);
+        }
+
+        [Test]
+        public void ClickOnLoginDerivativesPortal()
+        {
+            loginPage.GoToPage();
+            loginPage.ClickOnLoginButton();
+            Assert.That(loginPage.IsOnResultsPage(), Is.True);
+
+            //Click on SMA Portal Login Button
+            loginDerivativesPortal.ClickOnLoginButton();
+            Console.WriteLine("TEST");
+            Thread.Sleep(5000);
+
+            Assert.That(loginDerivativesPortal.IsOnResultsPage(), Is.True);
+        }
+
+        [Test]
+        public void addInvalidCredentials() 
+        {
+            loginPage.GoToPage();
+            loginPage.ClickOnLoginButton();
+            Assert.That(loginPage.IsOnResultsPage(), Is.True);
+
+            //Click on SMA Portal Login Button
+            loginDerivativesPortal.ClickOnLoginButton();
+            Console.WriteLine("TEST");
+            Thread.Sleep(5000);
+            Assert.That(loginDerivativesPortal.addInvalidCredentials("arojas", "pwd"), Is.EqualTo("Invalid login credentials"));
+        }
+    }
+}
