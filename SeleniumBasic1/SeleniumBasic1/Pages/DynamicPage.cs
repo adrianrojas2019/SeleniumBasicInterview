b3be2a525686cb0b4669da49733db619d70f255e
adrianrojas2019 <rojasadrian@hotmail.com>
2023-06-29 14:31:25 -0600

    updated

---
+++ SeleniumBasic1/SeleniumBasic1/Pages/DynamicPage.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumBasic1
+{
+    class DynamicPage
+    {
+        private IWebDriver driver;
+
+        public DynamicPage(IWebDriver _driver)
+        {
+            driver = _driver;
+        }
+
+        //public IWebElement SearchBox => driver.FindElement(By.CssSelector("input[title*=\"Search\"]"));
+        //public IWebElement SearchBox => driver.FindElement(By.Name("q"));
+        public IWebElement RemoveButton => driver.FindElement(By.XPath("//button[contains(text(),'Remove')]"));
+        //or the same below approach get property
+
+        //public IWebElement SearchBox
+        //{
+        //    get
+        //    {
+        //        return driver.FindElement(By.CssSelector("input[value*='Buscar con Google']"));
+
+        //    }
+        //}
+
+        //public IWebElement SearchButton => driver.FindElement(By.CssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.gNO89b"));
+        // getting two elements
+        //public IWebElement SearchButton => driver.FindElement(By.Name("btnK"));
+       //public IWebElement SearchButton => driver.FindElement(By.CssSelector("div[class='FPdoLc lJ9FBc'] input[name='btnK']"));
+        public IWebElement RemoveMessage => driver.FindElement(By.CssSelector("p[id='message']"));
+        public IWebElement EnabledButton => driver.FindElement(By.XPath("//*[@id='input-example']//button"));
+
+        private string url = "http://the-internet.herokuapp.com/dynamic_controls";
+
+        public void GoToPage()
+        {
+            driver.Navigate().GoToUrl(url);
+            //Maximize current window
+            driver.Manage().Window.Maximize();
+        }
+
+        public void ClickOnRemoteButton()
+        {
+            RemoveButton.Click();
+        }
+
+        public void ClickOnEnabledButton()
+        {
+            EnabledButton.Click();
+        }
+
+        public void IsMessageDisplayed(string message)
+        {
+            Assert.That(RemoveMessage.Text, Is.EqualTo(message));
+        }
+    }
+}
---
+++ SeleniumBasic1/SeleniumBasic1/Tests/DynamicControlTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SeleniumBasic1
+{
+    class DynamicControlTest : SeleniumTestBase
+    {
+        DynamicPage dynamicPage;
+
+        [OneTimeSetUp]
+        public void SetupOnce()
+        {
+            dynamicPage = new DynamicPage(driver);
+        }
+
+        [Test]
+        public void ClickOnRemoveButton()
+        {
+            dynamicPage.GoToPage();
+            dynamicPage.ClickOnRemoteButton();
+            Thread.Sleep(5000);
+            dynamicPage.IsMessageDisplayed("It's gone!");
+
+        }
+
+        [Test]
+        public void ClickOnEnabledButton()
+        {
+            dynamicPage.GoToPage();
+            dynamicPage.ClickOnEnabledButton();
+            Thread.Sleep(5000);
+            dynamicPage.IsMessageDisplayed("It's enabled!");
+        }
+    }
+}
